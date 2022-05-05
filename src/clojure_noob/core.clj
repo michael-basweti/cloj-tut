bbb847b5b1c82ab7159149c5d92c2ad937771644
michaelbasweti <baswetima@gmail.com>
2022-05-05 20:05:14 +0300

    initial commit

---
+++ src/clojure_noob/core.clj
@@ -0,0 +1,23 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!")
+  (println "Hi mikey")
+  (println "Joyie say, How are you doing??")
+  (println (+ 23 45))
+  (println (str "Hello " "my " "man"))
+
+  (def zeus "By Zeus's hammer!")
+  (def Aqua "By Aquaman trident")
+
+  (println zeus)
+
+;  (defn greeting
+;    "this is a greeting"
+;    [& args]
+;    (println "Joyie say, How are you doing??")
+;    )
+  )
