fda251c4187530200448c535c1b129ad1165f6a8
michaelbasweti <baswetima@gmail.com>
2022-05-06 11:30:40 +0300

    map

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -12,8 +12,25 @@
 
   (def zeus "By Zeus's hammer!")
   (def Aqua "By Aquaman trident")
+  (def cond "hey")
 
-  (println zeus)
+  (if (= cond "hey")
+    (println (* 1/2 10))
+    (println Aqua)
+    )
+
+  (def hmap {:firstname "Mike" :lastname "Basweti" :thirdname "Alex"})
+  (def examplemap (hash-map :subject1 "Math" :subject2 "Physics"))
+  (println hmap)
+  (println examplemap)
+
+;  get value using keyword
+  (println (get hmap :lastname))
+; get a key that doesn't exist, returns `nil`
+  (println (get hmap :lastname2))
+;  keyword example :thirdname
+
+  ;  (println zeus)
 
 ;  (defn greeting
 ;    "this is a greeting"
